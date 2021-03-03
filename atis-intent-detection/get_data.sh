b4920df62f05088c17403b33a83c33c222eb2e57
mtszkw <mateusz.kwasniak@pm.me>
2021-03-03 11:20:57 +0100

    Rename source directory, add get_data.sh script

---
+++ atis-intent-detection/get_data.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+pip install --upgrade kaggle
+
+# 811.74 KB
+# https://www.kaggle.com/hassanamin/atis-airlinetravelinformationsystem
+kaggle datasets download -d hassanamin/atis-airlinetravelinformationsystem
+
+# 4.68 GB
+# https://www.kaggle.com/yutanakamura/glove42b300dtxt
+kaggle datasets download -d yutanakamura/glove42b300dtxt
